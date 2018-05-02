9eaaae036448ffab98fd46e2845cbfb6624bfff9
Simon Kronstein <skronstein7@gmail.com>
2018-05-01 19:24:25 -0700

    Allow copying of files whose names contain spaces

--- src/camera.cpp
+++ src/camera.cpp
@@ -3090,7 +3090,7 @@ void recordEosCallback(void * arg)
 					/*qDebug()<<"recordEosCallback, camera->recorder->fileDirectory:" << camera->recorder->fileDirectory;*/
 
 					if(!(strcmp(mnt.mnt_dir, camera->recorder->fileDirectory))) continue; //if the destination directory is the one where the file was originally saved, dont copy the file over itself
-					sprintf(command, "cp %s %s", camera->recorder->path_full, mnt.mnt_dir);
+					sprintf(command, "cp \"%s\" \"%s\"", camera->recorder->path_full, mnt.mnt_dir);
 					//qDebug()<<"recordEosCallback, command:" << command;
 					int retval = system(command);
 					//qDebug()<<"recordEosCallback, retval:" << retval;
